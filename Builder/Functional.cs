5f71a774c9570e45c18d0d99702b61c933d3aef8
Rodrigo Gonzalez <vampiredd@live.com>
2025-01-07 20:08:03 -0600

    Builder

---
+++ Builder/Functional.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Builder
+{
+	public class Person
+	{
+		public string Name = string.Empty;
+		public string Position = string.Empty;
+	}
+
+	public sealed class PersonBuilder
+	{
+		public readonly List<Action<Person>> Actions
+		  = new List<Action<Person>>();
+
+		public PersonBuilder Called(string name)
+		{
+			Actions.Add(p => { p.Name = name; });
+			return this;
+		}
+
+		public Person Build()
+		{
+			var p = new Person();
+			Actions.ForEach(a => a(p));
+			return p;
+		}
+	}
+
+	public static class PersonBuilderExtensions
+	{
+		public static PersonBuilder WorksAsA
+		  (this PersonBuilder builder, string position)
+		{
+			builder.Actions.Add(p =>
+			{
+				p.Position = position;
+			});
+			return builder;
+		}
+	}
+
+	public static class Functional
+	{
+		public static void Run()
+		{
+			var pb = new PersonBuilder();
+			var person = pb.Called("Dmitri").WorksAsA("Programmer").Build();
+		}
+	}
+}
---
+++ Builder/Stepwise.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Builder
+{
+	public enum CarType
+	{
+		Sedan,
+		Crossover
+	};
+	public class Car
+	{
+		public CarType Type;
+		public int WheelSize;
+	}
+
+	public interface ISpecifyCarType
+	{
+		public ISpecifyWheelSize OfType(CarType type);
+	}
+
+	public interface ISpecifyWheelSize
+	{
+		public IBuildCar WithWheels(int size);
+	}
+
+	public interface IBuildCar
+	{
+		public Car Build();
+	}
+
+	public class CarBuilder
+	{
+		public static ISpecifyCarType Create()
+		{
+			return new Impl();
+		}
+
+		private class Impl :
+		  ISpecifyCarType,
+		  ISpecifyWheelSize,
+		  IBuildCar
+		{
+			private Car car = new Car();
+
+			public ISpecifyWheelSize OfType(CarType type)
+			{
+				car.Type = type;
+				return this;
+			}
+
+			public IBuildCar WithWheels(int size)
+			{
+				switch (car.Type)
+				{
+					case CarType.Crossover when size < 17 || size > 20:
+					case CarType.Sedan when size < 15 || size > 17:
+						throw new ArgumentException($"Wrong size of wheel for {car.Type}.");
+				}
+				car.WheelSize = size;
+				return this;
+			}
+
+			public Car Build()
+			{
+				return car;
+			}
+		}
+	}
+
+	internal static class Stepwise
+	{
+		public static void Run()
+		{
+			var car = CarBuilder.Create()
+				.OfType(CarType.Sedan)
+				.WithWheels(16)
+				.Build();
+			Console.WriteLine($"Car type: {car.Type}, wheel size: {car.WheelSize}");
+		}
+	}
+}
